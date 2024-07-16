c4710811fe1dc58a6ef8701bfa9ededb8e084aaa
pallaviwagh399 <swati.wagh399@gmail.com>
2024-07-16 23:18:28 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ class test
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program. my batch is 35, learning devops and aws.");
+        System.out.println("My First Java Program. my batch is 35, learning devops and aws. hiiii");
     }
 };
